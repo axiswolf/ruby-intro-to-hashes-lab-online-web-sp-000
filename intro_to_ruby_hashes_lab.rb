ba1beebe65676b65621330c564d7fd45cecb12ff
Karen V <karenvo@usc.edu>
2019-03-04 10:57:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,12 +9,12 @@ end
 
 def monopoly
 	monopoly =  {:railroads=>
-	  {:pieces=> [],
+	  {:pieces=>4,
 	    :rent_in_dollars=>
-	     {:one_piece_owned=>[],
-	      :two_pieces_owned=>[],
-	      :three_pieces_owned=>[],
-	      :four_pieces_owned=>[],
+	     {:one_piece_owned=>25,
+	      :two_pieces_owned=>50,
+	      :three_pieces_owned=>100,
+	      :four_pieces_owned=>200},
 	    :names=>
 	      {:reading_railroad=> {},
 	      :pennsylvania_railroad=> {},
